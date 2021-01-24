683a029790ed38e6e37f1ad846b18765a97b14f5
daniel simonson <dansimonson@mariposaweb.net>
2021-01-24 20:13:21 +0800

    removed unnecessary code; finished functionality to toggle ascending object array sort and display results

--- src/pages/Sort.js
+++ src/pages/Sort.js
@@ -28,6 +28,7 @@ const ContainerDiv = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 100px 0 0 0;
 `;
 
 const ButtonDisplay = styled.button`
@@ -71,77 +72,44 @@ const ButtonDisplay = styled.button`
   text-shadow: 0px 1px 0px #ffffff;
 `;
 
+const LabelHeader = styled.label`
+  margin: 50px 0 10px 0;
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
 function Sort() {
   const [sort, setSort] = useState(originalArray);
   const [ascSort, setAscSort] = useState(false);
   const [count, setCount] = useState(0);
-  const prevCountRef = useRef();
-  //const prevCount = 0prevCountRef.current;
-  useEffect(() => {
-    prevCountRef.current = count;
-  }, [count]);
-  const prevCount = prevCountRef.current;
-  const equals = (a, b) => {
-    a.length === b.length && a.every((v, i) => v === b[i]);
-  };
-  const toggleSort = () => {
-    setAscSort(!ascSort);
-    //keep track of even or odd click counts
-    if (count === 0) {
-      //count clicked first time, so set to 0
-      setCount(count + 1);
-    } else {
-      //increment count on each click
-      setCount(count + 1);
-      console.log("prevCount: ", prevCount);
-      console.log("count: ", count);
-    }
-  };
 
-  /*let prepareArray = (arr) => {
-    let tempArray = [];
-    arr.map((elem) => {
-      if (typeof elem === "object" && elem !== null) {
-        tempArray.push(JSON.stringify(elem));
-      } else {
-        tempArray.push(elem);
-      }
-    });
-    return tempArray;
-  };*/
-  const lna = () => {
+  const ascendingSort = () => {
     let sorted = [...sort];
     let remainder = count % 2;
-    console.log("remainder: ", remainder);
     if (remainder === 0) {
-      console.log("count is 0 or even");
+      //count is 0 or even
       sorted.sort((a, b) => (a.lastName > b.lastName ? 1 : -1));
       setSort(sorted);
       setAscSort(true);
       setCount(count + 1);
     } else {
-      console.log("count is odd");
+      //count is odd
       setSort(originalArray);
       setAscSort(false);
       setCount(count + 1);
     }
-    //let arrayTemp = prepareArray(sorted);
-    //sorted.sort((a, b) => (a.lastName > b.lastName ? 1 : -1));
-    //setSort(sorted);
-    /*let arraysSame = equals(sorted, originalArray);
-    if (arraysSame) {
-      sorted.sort((a, b) => (a.lastName > b.lastName ? 1 : -1));
-      setSort(sorted);
-    } else {
-      setSort(originalArray);
-    }*/
-    //toggleSort();
     return sorted;
   };
   return (
     <ContainerDiv>
-      <ButtonDisplay onClick={lna}>Toggle Ascending Sort</ButtonDisplay>
-      {ascSort ? <label>sorted array</label> : <label>pre sorted array</label>}
+      <ButtonDisplay onClick={ascendingSort}>
+        Toggle Ascending Sort
+      </ButtonDisplay>
+      {ascSort ? (
+        <LabelHeader>sorted array</LabelHeader>
+      ) : (
+        <LabelHeader>pre sorted array</LabelHeader>
+      )}
       {sort.map((person, index) => (
         <p key={index}>{person.lastName}</p>
       ))}
