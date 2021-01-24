5d80b209331f84f209b1a0ee9bbd2862c456cbbb
daniel simonson <dansimonson@mariposaweb.net>
2021-01-24 20:34:11 +0800

    added link to sort page from home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
+const ContainerDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const ButtonLink = styled.button`
+  text-align: center;
+  background-color: grey;
+  color: #fff;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 function Home() {
   return (
-    <div>
+    <ContainerDiv>
       <h1>Home</h1>
       <NavLink to="/sort">
-        <button>Go to Sort Page</button>
+        <ButtonLink>Go to Sort Page</ButtonLink>
       </NavLink>
-    </div>
+    </ContainerDiv>
   );
 }
 
